fe36b6d215e6a9e50580986b837a2b4d642b9ae4
DiegoFChC <diegofech15@gmail.com>
2023-10-26 13:01:20 -0500

    EW-15

--- src/services/contacts.post.js
+++ src/services/contacts.post.js
@@ -14,3 +14,18 @@ export async function newContacts(data) {
 
   return response;
 }
+
+export async function deleteContacts(data) {
+  const token = getCookie("Token");
+  const res = await fetch("http://127.0.0.1:8000/core/contacto/delete", {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: {
+      "Authorization": token,
+      "Content-Type": "application/json",
+    },
+  });
+  const response = await res.json();
+
+  return response;
+}
